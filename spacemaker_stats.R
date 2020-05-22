7c42414615910fcb8abe8175dea00b19abadb10b
adeepikaa <61487707+adeepikaa@users.noreply.github.com>
2020-05-22 00:11:56 -0400

    Update spacemaker_stats.R
    Added comments no changes to code

--- spacemaker_stats.R
+++ spacemaker_stats.R
@@ -3,41 +3,51 @@ library(RcppCNPy) ## for npyload
 library(Matrix) ## for nnzero
 library(dplyr) ## to use %>% 
 
-
+# Function to create features: coverage of road and coverage of building
 create_coverage <- function(x) {
-  input_path <- paste("data/", x, sep="")
+  input_path <- paste("data/", x, sep="") #create the path
   print(input_path)
-  sample_input <- npyLoad(input_path, "integer")
-  sample_fraction <- nnzero(sample_input)/length(sample_input)
+  sample_input <- npyLoad(input_path, "integer") # open the file
+  sample_fraction <- nnzero(sample_input)/length(sample_input) #calculate the coverage
   return(sample_fraction)
 }
+
+# Function to create building features as a whole
 create_bldg_stats <- function(x) {
   input_path <- paste("data/", x, sep="")
   print(input_path)
   sample_input <- npyLoad(input_path, "integer")
+  # calculate average building height(excludes the zeros)
   bldg_avg<-mean(sample_input[sample_input>0]) 
+  # calculate median building height(excludes the zeros)
   bldg_median<-median(sample_input[sample_input>0]) 
+  # calculate max building height(excludes the zeros)
   bldg_max<-max(sample_input) 
+  # calculate min building height(excludes the zeros)
   bldg_min<-min(sample_input[sample_input>0]) 
+  # calculate difference of max and min building height
   bldg_diff<-bldg_max-bldg_min
+  # return the aggregates as a vector
   c(bldg_avg=bldg_avg, bldg_median=bldg_median, 
     bldg_max=bldg_max, bldg_min=bldg_min, bldg_diff=bldg_diff)
 }
 
+# Function to create fetaures for multiple datasets
 create_stats_vec<- function(set, src, bldg) {
-  set$cov_rd <- sapply(set[,src],  create_coverage)
-  set$cov_bldg <- sapply(set[,bldg], create_coverage)
-  set$cov_ratio<-as.numeric(set$cov_bldg)/as.numeric(set$cov_rd)
-  set_stats<-sapply(set[,bldg], create_bldg_stats)
+  set$cov_rd <- sapply(set[,src],  create_coverage) # get road coverage
+  set$cov_bldg <- sapply(set[,bldg], create_coverage) # get building coverage
+  set$cov_ratio<-as.numeric(set$cov_bldg)/as.numeric(set$cov_rd) # ratio of building coverage to road coverage
+  set_stats<-sapply(set[,bldg], create_bldg_stats) # get building stats
   set_stats_t<-t(set_stats)
-  set_final<-cbind(set, set_stats_t)
-  return(set_final)
+  set_final<-cbind(set, set_stats_t) #merge building stats 
+  return(set_final) 
 }
 
 non_spec_final<-create_stats_vec(json_non_specific, "source_grid_path", "building_grid_path")
 spec_final<-create_stats_vec(json_specific, "source_grid_path", "building_grid_path")
 test_final<-create_stats_vec(json_test, "source_grid_path", "building_grid_path")
 
+# 
 non_spec_dist_stats<-mapply(create_dist_stats, json_non_specific$source_grid_path, json_non_specific$building_grid_path)
 spec_dist_stats<-mapply(create_dist_stats, json_specific$source_grid_path, json_specific$building_grid_path)
 test_dist_stats<-mapply(create_dist_stats, json_test$source_grid_path, json_test$building_grid_path)
@@ -49,3 +59,7 @@ non_spec_final_set<-cbind(non_spec_final, non_spec_dist_stats_t)
 spec_final_set<-cbind(spec_final, spec_dist_stats_t)
 test_final_set<-cbind(test_final, test_dist_stats_t)
 head(test_final_set)
+
+#rm(non_spec_final, spec_final, test_final, 
+#   non_spec_dist_stats, spec_dist_stats, test_dist_stats, 
+#   non_spec_dist_stats_t, spec_dist_stats_t, test_dist_stats_t)
