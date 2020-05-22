64434f6576a4e37ffe0869054ff4829f1a2b9bb0
adeepikaa <61487707+adeepikaa@users.noreply.github.com>
2020-05-22 00:09:35 -0400

    Update spacemaker_dist_stats.R
    Added lots of comments. There should not be any changes to actual code

--- spacemaker_dist_stats.R
+++ spacemaker_dist_stats.R
@@ -1,3 +1,6 @@
+# Function to create distance features
+# arguments get both *npy files src, bldg
+# for the purposes of the distance features, each building has been treated as a collection of buildings at adjacent locations
 create_dist_stats <- function(x,y) {
   src_path <- paste("data/", x, sep="")
   print(src_path)
@@ -5,6 +8,7 @@ create_dist_stats <- function(x,y) {
   bldg_path <- paste("data/", y, sep="")
   print(bldg_path)
   bldg_file <- npyLoad(bldg_path, "integer")
+  #initialize arr -array to save a list of locations that have noise source- to null
   arr<-NULL
   for (i in 1:51){
     for (j in 1:51) {
@@ -13,34 +17,37 @@ create_dist_stats <- function(x,y) {
       }   
     }
   }
-  
+  # define distance as the distance array that has distances between one location of building to all noise locations
   distance<-NULL
-  bldg_cnt<-0
-  D<-matrix(0, ncol=ncol(bldg_file), nrow=nrow(bldg_file))
+  D<-matrix(0, ncol=ncol(bldg_file), nrow=nrow(bldg_file)) 
+  # D is the matrix which has each building's minimum distance to road
   for (i in 1:51){
     for (j in 1:51) {
       if (bldg_file[i,j] > 0) {
         for (k in 1:nrow(arr)) {
-          distance[k]<-sqrt((arr[k,1]-i)^2+(arr[k,2]-j)^2)
+          distance[k]<-sqrt((arr[k,1]-i)^2+(arr[k,2]-j)^2) #calculate distance to the array of noise locations
         }
-        D[i,j]<-min(distance)
+        D[i,j]<-min(distance) #find minimum distance and save in D
       }
     }
   }
-  avg_dist_rd<-mean(D[D>0])
-  min_dist_rd<-min(D[D>0])
-  ht_min_dist_rd<-min(bldg_file[D==min_dist_rd])
+  avg_dist_rd<-mean(D[D>0]) # average of all minimum distance gives average distance from building to road
+  min_dist_rd<-min(D[D>0]) # minimum distance from building to road
+  ht_min_dist_rd<-min(bldg_file[D==min_dist_rd]) # height of closest building to road
   
+  # get building coverage for 4 buffer zones to portray density
   bldg_cov_rd_12<-sum(D[D<=12])/length(bldg_file)       # Zone 1 for distance less than 12 units
   bldg_cov_rd_24<-sum(D[D>12 & D<=24])/length(bldg_file) # Zone 2 for distance between 12 and 24 units
   bldg_cov_rd_36<-sum(D[D>24 & D<=36])/length(bldg_file) # Zone 3 for distance between 24 and 36 units
   bldg_cov_rd_51<-sum(D[D>36])/length(bldg_file)        # Zone 4 for distance greater than 36 units
   
+  # get average building height for 4 buffer zones 
   ht_avg_dist_rd_12<-ifelse(bldg_cov_rd_12!=0, mean(bldg_file[D>0 & D<=12]), 0)
   ht_avg_dist_rd_24<-ifelse(bldg_cov_rd_24!=0, mean(bldg_file[D>12 & D<=24]), 0)
   ht_avg_dist_rd_36<-ifelse(bldg_cov_rd_36!=0, mean(bldg_file[D>24 & D<=36]), 0)
   ht_avg_dist_rd_51<-ifelse(bldg_cov_rd_51!=0, mean(bldg_file[D>36]), 0)
   
+  # get maximum building height for 4 buffer zones 
   ht_max_dist_rd_12<-ifelse(bldg_cov_rd_12!=0, max(bldg_file[D>0 & D<=12]), 0)
   ht_max_dist_rd_24<-ifelse(bldg_cov_rd_24!=0, max(bldg_file[D>12 & D<=24]), 0)
   ht_max_dist_rd_36<-ifelse(bldg_cov_rd_36!=0, max(bldg_file[D>24 & D<=36]), 0)
