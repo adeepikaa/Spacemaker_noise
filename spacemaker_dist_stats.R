0f5a06a4066f1c66c7bccf21d0afe4bfcfb9b36a
Lois Dankwa <loisdankwa@yahoo.com>
2020-05-28 15:48:29 -0400

    I just edited and added a bit more comments to be self-explanatory.

--- spacemaker_dist_stats.R
+++ spacemaker_dist_stats.R
@@ -1,6 +1,18 @@
+#######################
+
+
+
+# This file creates all the distance related features for the Spacemaker noise surrogate model
+
+
+
 # Function to create distance features
-# arguments get both *npy files src, bldg
-# for the purposes of the distance features, each building has been treated as a collection of buildings at adjacent locations
+
+# arguments get both *npy files src (source), bldg(building)
+
+# for the purposes of the distance features, each building has been treated as a collection of 
+# buildings at adjacent locations
+
 create_dist_stats <- function(x,y) {
   src_path <- paste("data/", x, sep="")
   print(src_path)
@@ -8,7 +20,7 @@ create_dist_stats <- function(x,y) {
   bldg_path <- paste("data/", y, sep="")
   print(bldg_path)
   bldg_file <- npyLoad(bldg_path, "integer")
-  #initialize arr -array to save a list of locations that have noise source- to null
+  #initialize arr (array) to save a list of locations that have noise source- to null
   arr<-NULL
   for (i in 1:51){
     for (j in 1:51) {
@@ -17,41 +29,60 @@ create_dist_stats <- function(x,y) {
       }   
     }
   }
+  
+  
+  
+  
   # define distance as the distance array that has distances between one location of building to all noise locations
-  distance<-NULL
-  D<-matrix(0, ncol=ncol(bldg_file), nrow=nrow(bldg_file)) 
+  
+  distance <- NULL
+  D <- matrix(0, ncol=ncol(bldg_file), nrow=nrow(bldg_file)) 
+  
   # D is the matrix which has each building's minimum distance to road
   for (i in 1:51){
     for (j in 1:51) {
       if (bldg_file[i,j] > 0) {
         for (k in 1:nrow(arr)) {
-          distance[k]<-sqrt((arr[k,1]-i)^2+(arr[k,2]-j)^2) #calculate distance to the array of noise locations
+          distance[k]<-sqrt((arr[k,1]-i)^2+(arr[k,2]-j)^2)    #calculate distance to the array of noise locations
         }
-        D[i,j]<-min(distance) #find minimum distance and save in D
+        D[i,j]<-min(distance)        #find minimum distance and save in D
       }
     }
   }
-  avg_dist_rd<-mean(D[D>0]) # average of all minimum distance gives average distance from building to road
-  min_dist_rd<-min(D[D>0]) # minimum distance from building to road
+  
+  
+  avg_dist_rd<-mean(D[D>0])              # average of all minimum distance gives average distance from building to road
+  min_dist_rd<-min(D[D>0])               # minimum distance from building to road
   ht_min_dist_rd<-min(bldg_file[D==min_dist_rd]) # height of closest building to road
   
-  # get building coverage for 4 buffer zones to portray density
-  bldg_cov_rd_12<-sum(D[D<=12])/length(bldg_file)       # Zone 1 for distance less than 12 units
-  bldg_cov_rd_24<-sum(D[D>12 & D<=24])/length(bldg_file) # Zone 2 for distance between 12 and 24 units
-  bldg_cov_rd_36<-sum(D[D>24 & D<=36])/length(bldg_file) # Zone 3 for distance between 24 and 36 units
-  bldg_cov_rd_51<-sum(D[D>36])/length(bldg_file)        # Zone 4 for distance greater than 36 units
   
-  # get average building height for 4 buffer zones 
+  
+  
+  # get building coverage for 4 buffer zones to portray density from road
+  
+  bldg_cov_rd_12<-sum(D[D<=12])/length(bldg_file)           # Zone 1 for distance less than 12 units
+  bldg_cov_rd_24<-sum(D[D>12 & D<=24])/length(bldg_file)    # Zone 2 for distance between 12 and 24 units
+  bldg_cov_rd_36<-sum(D[D>24 & D<=36])/length(bldg_file)    # Zone 3 for distance between 24 and 36 units
+  bldg_cov_rd_51<-sum(D[D>36])/length(bldg_file)            # Zone 4 for distance greater than 36 units
+  
+  
+  
+  # get average building heights within the for 4 different buffer zones 
+  
   ht_avg_dist_rd_12<-ifelse(bldg_cov_rd_12!=0, mean(bldg_file[D>0 & D<=12]), 0)
   ht_avg_dist_rd_24<-ifelse(bldg_cov_rd_24!=0, mean(bldg_file[D>12 & D<=24]), 0)
   ht_avg_dist_rd_36<-ifelse(bldg_cov_rd_36!=0, mean(bldg_file[D>24 & D<=36]), 0)
   ht_avg_dist_rd_51<-ifelse(bldg_cov_rd_51!=0, mean(bldg_file[D>36]), 0)
   
-  # get maximum building height for 4 buffer zones 
+  
+  # get maximum building heights within the 4 different buffer zones 
+  
   ht_max_dist_rd_12<-ifelse(bldg_cov_rd_12!=0, max(bldg_file[D>0 & D<=12]), 0)
   ht_max_dist_rd_24<-ifelse(bldg_cov_rd_24!=0, max(bldg_file[D>12 & D<=24]), 0)
   ht_max_dist_rd_36<-ifelse(bldg_cov_rd_36!=0, max(bldg_file[D>24 & D<=36]), 0)
   ht_max_dist_rd_51<-ifelse(bldg_cov_rd_51!=0, max(bldg_file[D>36]), 0)
+  
+  
   
   return(c(avg_dist_rd=avg_dist_rd, min_dist_rd=min_dist_rd, ht_min_dist_rd=ht_min_dist_rd, 
            cov_buffer1=bldg_cov_rd_12,     cov_buffer2=bldg_cov_rd_24, #cov_buffer* measures the building covereage in the different zones
@@ -63,3 +94,17 @@ create_dist_stats <- function(x,y) {
          )
 }
 
+
+# Creating the distance features for non-specific, specific and test data
+
+non_spec_dist_stats<-mapply(create_dist_stats, json_non_specific$source_grid_path, json_non_specific$building_grid_path)
+spec_dist_stats<-mapply(create_dist_stats, json_specific$source_grid_path, json_specific$building_grid_path)
+test_dist_stats<-mapply(create_dist_stats, json_test$source_grid_path, json_test$building_grid_path)
+
+
+
+# Switching the columns and the rows
+
+non_spec_dist_stats_t<-t(non_spec_dist_stats)
+spec_dist_stats_t<-t(spec_dist_stats)
+test_dist_stats_t<-t(test_dist_stats)
--- spacemaker_read_data.R
+++ spacemaker_read_data.R
@@ -1,21 +1,53 @@
-library(jsonlite) ## for json read
-library(stringr) ## for str split
-library(tidyr) ## for spread function
+#######################
 
-#Read the JSON file. The JSON file has to be inside data directory in the working directory for this code to work
+
+
+# This file reads all the data related to the Spacemaker noise surrogate model from the data and 
+# solution folders
+
+
+# Installing and reading packages
+
+install.packages("jsonlite")
+install.packages("stringr")
+install.packages("tidyr")
+
+library(jsonlite)       ## for json read
+library(stringr)        ## for str split
+library(tidyr)          ## for spread function
+
+
+#PART 1: 
+
+#Reading the non-specific training data
+
 json1<-fromJSON(txt="data/non_specific_training_data.json")
+
+
+# Using the unlist function to change the list format and to add the data as rows
 json1<-data.frame(number = unlist(json1))
-# the unlist function changes the list format and adds data as rows. 
+
+
+
 # Also gives rownames that have all the strings attached by "."
 # Using strsplit by . and transform into columns
+
 json1$scenario<-lapply(strsplit(as.character(rownames(json1)),"\\."), "[", 1)
 json1$id<-lapply(strsplit(as.character(rownames(json1)), "\\."), "[", 2)
 json1$path<-lapply(strsplit(as.character(rownames(json1)), "\\."), "[", 3)
 rownames(json1)<-NULL
+
+
 # Use spread function from tidyr to change rows into columns by specific to each entry
+
 json_non_specific<-spread(json1, path, number)
 json_non_specific$fraction_yellow_zone <- as.numeric(as.character(json_non_specific$fraction_yellow_zone))
 
+
+
+# PART 2: 
+
+# Reading the specific training data
 
 json2<-fromJSON(txt="data/specific_training_data.json")
 json2<-data.frame(number = unlist(json2))
@@ -25,6 +57,11 @@ rownames(json2)<-NULL
 json_specific<-spread(json2, path, number)
 json_specific$fraction_yellow_zone <- as.numeric(as.character(json_specific$fraction_yellow_zone))
 
+
+
+# PART 3: 
+
+# Reading the test data
 
 json3<-fromJSON(txt="data/test_data.json")
 json3<-data.frame(number = unlist(json3))
